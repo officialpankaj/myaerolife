8dd65d496364738eb9edc9ff239f36fe856b22cc
Pankaj Barman <pankajbarman000@gmail.com>
2026-01-04 17:43:26 +0530

    add: initial project setup with vite+tailwind+react

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1>My Aero Life</h1>
+      <div className="card">
+        <p>Website coming soon!</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
