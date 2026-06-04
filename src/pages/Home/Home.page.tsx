df5da739cb267419d720aec15e1a93cdd232f804
Pankaj Barman <pankajbarman000@gmail.com>
2026-06-04 23:10:39 +0530

    update: home page video with vimeo iframe video

--- src/pages/Home/Home.page.tsx
+++ src/pages/Home/Home.page.tsx
@@ -2,10 +2,13 @@ import Header from "../../components/Header/Header.component";
 
 const Home = () => {
   return (
-    <div className="flex h-screen flex-col">
-      <Header />
-      <div className="flex h-screen flex-1 flex-col md:flex-row">
-        <video
+    <div className="relative flex h-screen flex-col">
+      <div className="top-0 left-0 w-full md:absolute">
+        <Header />
+      </div>
+
+      <div className="flex h-screen w-full flex-1 flex-col md:flex-row">
+        {/* <video
           className="h-full w-full object-cover md:w-1/2"
           muted
           loop
@@ -18,7 +21,16 @@ const Home = () => {
           loop
           autoPlay
           src="/videos/hero-video2.mp4"
-        />
+        /> */}
+        <div className="w-full">
+          <iframe
+            src="https://player.vimeo.com/video/1198482616?controls=0&badge=0&autopause=0&player_id=0&app_id=58479&autoplay=1"
+            allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            className="aspect-video w-full"
+            title="2JuneAerolifeMiniUsageFilm"
+          />
+        </div>
       </div>
     </div>
   );
